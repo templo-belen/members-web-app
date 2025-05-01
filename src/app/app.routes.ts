688492335080d61bb4e48d94082ddfaeff02e06c
Pedro Gonzalez <1pedrog@gmail.com>
2025-05-01 11:00:32 -0500

    Adding page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,20 @@ import { Routes } from '@angular/router';
 import { MembersComponent } from './shared/components/members/members.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './shared/components/login/login.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/members', pathMatch: 'full' },
-  { path: 'members', component: MembersComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'app',
+    component: LayoutComponent,
+    //canActivate: [AuthGuard],
+    children: [
+      // TODO: ajustar optiones del menú
+      { path: 'members', component: MembersComponent },
+      { path: 'member-details', component: MembersComponent },
+      { path: '', redirectTo: 'member-details', pathMatch: 'full' }
+    ]
+  },
 ];
---
+++ src/app/shared/components/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterModule, ClarityModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
