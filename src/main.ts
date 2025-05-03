f376c81fa4467a49a88a28641a8dc1f4e1221fb2
unknown <erika.programmer@gmail.com>
2025-05-03 12:33:37 -0500

    Adding icons for tabs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {loadCoreIconSet,
+  loadEssentialIconSet,
+} from '@cds/core/icon';
+
+loadCoreIconSet();
+loadEssentialIconSet();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
