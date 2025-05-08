af4233c4d41c084c7e4151ebcce47ed6e8e4ed6c
PSOFKA01275\daniel.ramirez <daniel.ramirez@sofka.com.co>
2025-05-08 17:34:38 -0500

    adding reference form

--- src/app/shared/components/member-details/references/references-member-details.component.ts
+++ src/app/shared/components/member-details/references/references-member-details.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClrFormsModule } from '@clr/angular';
 @Component({
   selector: 'app-references-member-details',
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule, ClrFormsModule, CommonModule],
   templateUrl: './references-member-details.component.html',
-  styleUrl: './references-member-details.component.scss'
-})
-export class ReferencesMemberDetailsComponent {
+  styleUrl: './references-member-details.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true
 
+})
+export class ReferencesMemberDetailsComponent implements OnInit {
+  //TODO: mover a un form group 
+  time = new FormControl('');
+  churchName = new FormControl('');
+  leadPastorName = new FormControl('');
+  reasonForLeaving = new FormControl('');
+  reasonsForJoining = new FormControl('');
+  createdOn = new FormControl('');
+  createdBy = new FormControl('');
+  modifiedOn = new FormControl('');
+  modifiedBy = new FormControl('');
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const formData = {
+    };    
+  }
 }
