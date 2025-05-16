7cd3e6620884b050b36fc8e7516211b151be5b8f
PSOFKA01275\daniel.ramirez <daniel.ramirez@sofka.com.co>
2025-05-16 18:39:09 -0500

    removed imports

--- src/app/shared/components/members/details/references/references-member-details.component.ts
+++ src/app/shared/components/members/details/references/references-member-details.component.ts
@@ -1,80 +1,85 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, FormArray, FormControl } from '@angular/forms';
-import { ClrFormsModule, ClrDatagridModule, ClrIconModule } from '@clr/angular';
-import { MemberReferences, MemberReference } from '../../../../../core/models/member.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormArray } from '@angular/forms';
+import { ClrFormsModule, ClrIconModule } from '@clr/angular';
+import {MemberReferences,MemberReference,} from '../../../../../core/models/member.model';
 import { MemberService } from '../../../../../core/services/member.service';
 import { editModeSubject } from '../../../../../core/subjects/members.subjects';
 
 @Component({
   selector: 'app-references-member-details',
-  imports: [FormsModule, ReactiveFormsModule, ClrFormsModule, ClrDatagridModule, ClrIconModule, CommonModule],
+  standalone: true,
+  imports: [ReactiveFormsModule, ClrFormsModule, ClrIconModule, CommonModule],
   templateUrl: './references-member-details.component.html',
   styleUrl: './references-member-details.component.scss',
 })
 export class ReferencesMemberDetailsComponent implements OnInit {
-
   private _memberService = inject(MemberService);
 
   isEditable: boolean = false;
   memberForm: FormGroup;
   memberReferences: MemberReferences | null = null;
 
-    constructor(private fb: FormBuilder) {
-      this.memberForm = this.buildForm(new MemberReferences());
-      this.setFormEditable();
-    }
+  constructor(private fb: FormBuilder) {
+    this.memberForm = this.buildForm(new MemberReferences());
+    this.setFormEditable();
+  }
 
-    ngOnInit(): void {
-    editModeSubject.subscribe(mode => {
+  ngOnInit(): void {
+    editModeSubject.subscribe((mode) => {
       this.isEditable = mode;
       this.setFormEditable();
     });
-    this._memberService.fetchSelectedMemberId().subscribe(memberId => {
-        this._memberService.dispatchMemberReferences(memberId);
+
+    this._memberService.fetchSelectedMemberId().subscribe((memberId) => {
+      this._memberService.dispatchMemberReferences(memberId);
     });
-    
-    this._memberService.fetchMemberReferences().subscribe(memberReferences => {
+
+    this._memberService.fetchMemberReferences().subscribe((memberReferences) => {
       this.memberForm = this.buildForm(memberReferences);
-        this.setFormEditable();
+      this.setFormEditable();
     });
+
     this.memberForm.disable();
   }
 
- buildForm(memberReferences: MemberReferences): FormGroup {
+  buildForm(memberReferences: MemberReferences): FormGroup {
     const referencesArray = this.fb.array(
-      (memberReferences.references || []).map(ref => this.createReferenceGroup(ref))
+      (memberReferences.references || []).map((ref) =>
+        this.createReferenceGroup(ref)
+      )
     );
 
     return this.fb.group({
       references: referencesArray,
-      reasonsForCongregating: [memberReferences.reasonsForCongregating || '']
-    });
-  }
-  createForm(): FormGroup {
-    return this.fb.group({
-      id: [0],
-      references: this.fb.array([]),
-      reasonsForCongregating: ['']
+      reasonsForCongregating: [memberReferences.reasonsForCongregating || ''],
     });
   }
 
-  setFormEditable() {
+  createForm(): FormGroup {
+    return this.fb.group({
+      references: this.fb.array([]),
+      reasonsForCongregating: [''],
+    });
+  }
+
+  setFormEditable(): void {
     if (this.isEditable) {
       this.memberForm.enable();
     } else {
       this.memberForm.disable();
     }
   }
+
+  toggleEditMode(): void {
+    editModeSubject.next(!this.isEditable);
+  }
+
   get referencesArray(): FormArray {
     return this.memberForm.get('references') as FormArray;
   }
 
- toggleEditMode() {
-    editModeSubject.next(!this.isEditable);
-  } 
-
- addReference(): void {
+  addReference(): void {
     this.referencesArray.push(this.createReferenceGroup());
   }
 
@@ -87,8 +92,10 @@ export class ReferencesMemberDetailsComponent implements OnInit {
       totalTime: [reference.totalTime || ''],
       churchName: [reference.churchName || ''],
       mainPastorName: [reference.mainPastorName || ''],
-      leavingReason: [reference.leavingReason || '']
+      leavingReason: [reference.leavingReason || ''],
     });
   }
-    onSubmit() {}
+
+  onSubmit() {
+  }
 }
