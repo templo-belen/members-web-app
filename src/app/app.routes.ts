84a303f17dde82c75f87c15ccdcda4d2c9c2de06
Francisco Javier Rosas Jimenez <frjrosas@gmail.com>
2025-05-28 22:43:12 -0500

    feat: Eliminacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,13 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './shared/components/login/login.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
-import { layoutRoutes } from './shared/components/layout/layout.routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'app',
+    path: 'members',
     component: LayoutComponent,
     canActivate: [AuthGuard],
-    children: layoutRoutes
   },
 ];
--- src/app/shared/components/members/details/member-details.component.ts
+++ src/app/shared/components/members/details/member-details.component.ts
@@ -1,31 +1,38 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router, RouterModule} from '@angular/router';
+import {Component, inject, input, OnInit, output} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {ClrVerticalNavModule} from '@clr/angular';
 import {editModeSubject} from '../../../../core/subjects/members.subjects';
 import {CommonModule} from '@angular/common';
 import {MemberService} from '../../../../core/services/member.service';
+import {BasicInfoMemberDetailsComponent} from './basic-info/basic-info-member-details.component';
+import {FamilyInfoMemberDetailsComponent} from './family-info/family-info-member-details.component';
+import {GeneralInfoMemberDetailsComponent} from './general-info/general-info-member-details.component';
+import {ReferencesMemberDetailsComponent} from './references/references-member-details.component';
+import {DewInfoMemberDetailsComponent} from './dew-info/dew-info-member-details.component';
 
 @Component({
   selector: 'app-member-details',
-  imports: [RouterModule, ClrVerticalNavModule, CommonModule],
+  imports: [RouterModule, ClrVerticalNavModule, CommonModule, BasicInfoMemberDetailsComponent, FamilyInfoMemberDetailsComponent, GeneralInfoMemberDetailsComponent, ReferencesMemberDetailsComponent, DewInfoMemberDetailsComponent],
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.scss',
 })
 export class MemberDetailsComponent implements OnInit {
-  @Input({ required: true }) closeModal!: () => void;
-  @Input({ required: true }) memberName!: string;
-  @Input({ required: true }) memberId!: number;
+  closeModal = output<void>();
+  memberId = input.required<number>();
+  memberName = input.required<string>();
+
 
   isEditable = false;
+  selected = "basicInfo";
   currentTabTitle = 'Datos Personales';
 
   private _memberService = inject(MemberService);
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor() {
     editModeSubject.subscribe(mode => this.isEditable = mode);
   }
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this._memberService.dispatchMemberFormValues();
   }
 
@@ -34,24 +41,12 @@ export class MemberDetailsComponent implements OnInit {
   }
 
   handleClose() {
-    this.closeModal();
+    this.closeModal.emit();
     this.currentTabTitle = 'Datos Personales';
     editModeSubject.next(false);
-    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
-  onLinkClick(event: MouseEvent, path: string): void {
-    event.preventDefault();
-    event.stopPropagation();
-
-    if (this.isEditable) {
-      return;
-    }
-
-    this.router.navigate([path], { relativeTo: this.route });
-
-    const element = event.currentTarget as HTMLElement;
-    this.currentTabTitle = element.textContent?.trim() ?? '';
+  select(option: string) {
+    this.selected = option;
   }
-
 }
