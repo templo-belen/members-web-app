77a38a53e7567701e22d8032284dd038d7db12b1
Pedro Gonzalez <1pedrog@gmail.com>
2025-05-30 17:46:31 -0500

    Showing error messages on basic info form

--- src/app/shared/components/members/details/basic-info/basic-info-member-details.component.ts
+++ src/app/shared/components/members/details/basic-info/basic-info-member-details.component.ts
@@ -1,11 +1,11 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ClarityModule} from '@clr/angular';
-import {MemberBasicInfo, MemberFormValues} from '../../../../../core/models/member.model';
-import {CommonModule} from '@angular/common';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MemberService} from '../../../../../core/services/member.service';
-import {editModeSubject} from '../../../../../core/subjects/members.subjects';
-import {EnumResponseModel} from '../../../../../core/models/enum.model';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { ClarityModule, ClrForm } from '@clr/angular';
+import { MemberBasicInfo, MemberFormValues } from '../../../../../core/models/member.model';
+import { CommonModule } from '@angular/common';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MemberService } from '../../../../../core/services/member.service';
+import { editModeSubject } from '../../../../../core/subjects/members.subjects';
+import { EnumResponseModel } from '../../../../../core/models/enum.model';
 
 @Component({
   selector: 'app-basic-info-member-details',
@@ -17,6 +17,8 @@ export class BasicInfoMemberDetailsComponent implements OnInit {
 
   private _memberService = inject(MemberService);
 
+  @ViewChild('formElement') formElement!: ElementRef<HTMLFormElement>;
+
   memberId: number = 0;
   basicInfoForm: FormGroup;
   isEditable: boolean = false;
@@ -26,6 +28,10 @@ export class BasicInfoMemberDetailsComponent implements OnInit {
     preachingPoints: []
   }
 
+  // TODO: replace with key in the HTML when applying i18n.
+  requiredFieldError = 'Este campo es obligatorio';
+  invalidFormatError = 'El formato es incorrecto';
+
   constructor(private fb: FormBuilder) {
     this.basicInfoForm = this.buildForm(new MemberBasicInfo());
     this.setFormEditable();
@@ -34,9 +40,27 @@ export class BasicInfoMemberDetailsComponent implements OnInit {
   buildForm(memberBasicInfo: MemberBasicInfo): FormGroup {
     const form = this.fb.group({
       ...memberBasicInfo,
+      idNumber: new FormControl(memberBasicInfo.idNumber),
+      names: new FormControl(memberBasicInfo.names),
+      surnames: new FormControl(memberBasicInfo.surnames),
+      currentRole: new FormControl(memberBasicInfo.currentRole),
+      cellLeadership: new FormControl(memberBasicInfo.cellLeadership),
+      leadership: new FormControl(memberBasicInfo.leadership),
+      email: new FormControl(memberBasicInfo.email),
+      phoneNumber: new FormControl(memberBasicInfo.phoneNumber),
+      cellphoneNumber: new FormControl(memberBasicInfo.cellphoneNumber),
       file: new FormControl<FileList | undefined>(undefined),  //TODO evaluar si formara parte del modelo
     });
 
+    // Required fields
+    form.get('idNumber')?.addValidators(Validators.required);
+    form.get('names')?.addValidators(Validators.required);
+    form.get('surnames')?.addValidators(Validators.required);
+    form.get('currentRole')?.addValidators(Validators.required);
+    form.get('cellLeadership')?.addValidators(Validators.required);
+    form.get('leadership')?.addValidators(Validators.required);
+
+    // Additional validations
     const phoneRegex = /^(\+\d{1,3})?(\s\(\d{3}\)\s|\s?\d{3}[\s-]?)\d{3}[\s-]?\d{4,6}$/;
     form.get('phoneNumber')?.addValidators(Validators.pattern(phoneRegex));
     form.get('cellphoneNumber')?.addValidators(Validators.pattern(phoneRegex));
@@ -74,6 +98,8 @@ export class BasicInfoMemberDetailsComponent implements OnInit {
   }
 
   onSubmit() {
+    this.triggerFocusAndBlurEvents();
+
     if (!this.basicInfoForm.valid) {
       return;
     }
@@ -83,6 +109,20 @@ export class BasicInfoMemberDetailsComponent implements OnInit {
     } else {
       // TODO: implement update logic
     }
+  }
+
+  // Trick for triggering all error messages
+  private triggerFocusAndBlurEvents() {
+    const elements = this.formElement
+      .nativeElement
+      .querySelectorAll('input, select');
+
+    elements.forEach((element: any) => {
+      if (element.focus && element.blur) {
+        element.focus();
+        element.blur();
+      }
+    });
   }
 
   toggleEditMode() {
