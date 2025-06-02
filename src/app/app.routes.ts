f0bf51bdadc26560d1c906261e4c467f4e901335
Pedro Gonzalez <1pedrog@gmail.com>
2025-06-01 20:27:35 -0500

    Final touches

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './shared/components/login/login.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
+import { layoutRoutes } from './shared/components/layout/layout.routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'members',
+    path: 'app',
     component: LayoutComponent,
     canActivate: [AuthGuard],
+    children: layoutRoutes
   },
   // Always keep at last route
   // TODO: define a 404 page
