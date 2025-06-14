de78a028c83f32d8175d99618f94176f4de5d15d
ErisoHV <erika.programmer@gmail.com>
2025-06-13 19:30:03 -0500

    Adding warnings

--- eslint.config.js
+++ eslint.config.js
@@ -14,8 +14,8 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
       "@angular-eslint/directive-selector": [
         "error",
         {
